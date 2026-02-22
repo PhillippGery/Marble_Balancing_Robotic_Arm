83dd1f2b03cb4669616fbb6ddfbbc36f3ff4f75f
Kevin Biju Mathew <kb@purdue.edu>
2026-02-22 15:49:12 -0500

    Spec

--- 40_Simulation/cs558/build/OldOne_description/prefix_override/sitecustomize.py
+++ 40_Simulation/cs558/build/OldOne_description/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/lincoln/cs558/install/OldOne_description'
+    sys.prefix = sys.exec_prefix = '/home/lincoln/Marble_Balancing_Robotic_Arm/40_Simulation/cs558/install/OldOne_description'
