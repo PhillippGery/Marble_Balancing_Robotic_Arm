3db7e7f5d2efc8389f20984ce1f2f91b990be1a1
Kevin Biju Mathew <kb@purdue.edu>
2026-03-19 22:50:36 -0400

    switch

---
+++ 40_Simulation/cs558/build/ur_simulation_gazebo/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/lincoln/Marble_Balancing_Robotic_Arm/40_Simulation/cs558/install/ur_description/share/ur_description/package.sh"
